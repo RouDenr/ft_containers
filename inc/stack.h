17915f1f5583b77eeef49c4e23b946c9ec7d2ed3
decordel <decordel@student.21-school.ru>
2022-09-06 17:43:14 +0300

    add stack

---
+++ inc/stack.h
@@ -0,0 +1,44 @@
+// Copyright 2022 by decordel
+
+#ifndef __STACK_H__
+# define __STACK_H__
+#include "vector.hpp"
+
+namespace ft {
+    template <typename T, typename Container = vector<T> >
+    class stack {
+        typedef Container container_type;
+        typedef typename Container::value_type      value_type;
+        typedef typename Container::size_type       size_type;
+        typedef typename Container::reference       reference;
+        typedef typename Container::const_reference const_reference;
+
+        stack() : c() {}
+        explicit stack(const Container& cont = Container());
+        stack( const stack& other ) : c(other.c) {}
+        ~stack() {}
+
+        stack& operator=(const stack& other) {
+            this->c = other.c;
+        }
+        reference top() { return c.back(); }
+        const_reference top() const { return c.back(); }
+
+        bool empty() const { return c.empty(); }
+        size_type size() const { return c.size(); }
+        void push( const value_type& value ) { c.push_back(value); }
+        void pop() { c.pop_back(); }
+
+        bool operator==(const stack<T,Container>& rhs ) { return this->c == rhs.c;}
+        bool operator!=(const stack<T,Container>& rhs ) { return this->c != rhs.c;}
+        bool operator< (const stack<T,Container>& rhs ) { return this->c < rhs.c;}
+        bool operator<=(const stack<T,Container>& rhs ) { return this->c <= rhs.c;}
+        bool operator> (const stack<T,Container>& rhs ) { return this->c > rhs.c;}
+        bool operator>=(const stack<T,Container>& rhs ) { return this->c >= rhs.c;}
+
+    protected:
+        Container c;
+    };
+}
+
+#endif //__STACK_H__
