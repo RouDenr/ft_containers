d10bc6ddd02c690198f966220ec83c0bed1482e4
decordel <decordel@student.21-school.ru>
2022-09-15 21:13:40 +0300

    add: vector: tabs :)

--- inc/vector.hpp
+++ inc/vector.hpp
@@ -9,14 +9,14 @@ namespace ft {
     template <typename T, typename Allocator = std::allocator<T> >
     class vector {
     public:
-        typedef T value_type;
-        typedef Allocator allocator_type;
-        typedef typename std::ptrdiff_t difference_type;
-        typedef value_type& reference;
-        typedef const value_type& const_reference;
-        typedef typename Allocator::pointer pointer;
-        typedef typename Allocator::const_pointer const_pointer;
-        typedef typename std::size_t size_type;
+        typedef T                                   value_type;
+        typedef Allocator                           allocator_type;
+        typedef typename std::ptrdiff_t             difference_type;
+        typedef value_type&                         reference;
+        typedef const value_type&                   const_reference;
+        typedef typename Allocator::pointer         pointer;
+        typedef typename Allocator::const_pointer   const_pointer;
+        typedef typename std::size_t                size_type;
 
         class const_iterator;
         class iterator;
